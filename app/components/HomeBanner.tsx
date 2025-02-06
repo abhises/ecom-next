c82152629a7ffb5d859f2ee9b608e0577fb6e078
abhises <abhisespoudyal@gmail.com>
2025-02-06 17:44:56 +0545

    first commit

---
+++ app/components/HomeBanner.tsx
@@ -0,0 +1,16 @@
+const HomeBanner = () => {
+  return (
+    <div className="relative bg-gradient-to-r from-sky-500 to-sky-700 mb-8">
+      <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center justify-evenly">
+        <div>
+          <h1>Summer Sale!</h1>
+          <p>Enjoy discount on selected items</p>
+          <p>GET 50% OFF</p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeBanner;
