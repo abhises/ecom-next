1ebf7fee7ac3bc76a30da0f55629c34d63c97b0b
abhises <abhisespoudyal@gmail.com>
2025-02-22 21:21:26 +0545

    nav

--- app/components/nav/UserMenu.tsx
+++ app/components/nav/UserMenu.tsx
@@ -3,18 +3,41 @@
 import { useCallback, useState } from "react";
 import Avatar from "../Avatar";
 import { AiFillCaretDown } from "react-icons/ai";
+import Link from "next/link";
 
 const UserMenu = () => {
-  const [iseOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = useCallback(() => {
     setIsOpen((prev) => !prev);
   }, []);
   return (
     <div className="relative z-30">
-      <div onClick={toggleOpen}>
+      <div
+        onClick={toggleOpen}
+        className="p-1 border-[1px]
+      border-slate-400 flex flex-row
+      items-center
+      gap-1
+      rounded-full
+      cursor-pointer
+      hover:shadow-md
+      transition
+      text-slate-700
+      ">
         <Avatar />
         <AiFillCaretDown />
       </div>
+      {isOpen && (
+        <div
+          className="absolute rounded-md shadow-md w-[170px]
+        bg-white overflow-hidden right-0 top-12 text-sm flex flex-col
+        cursor-pointer
+        ">
+          <div>
+            <Link href="orders"></Link>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
